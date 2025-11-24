e66291e38db68f66dda298d116c00aa17fb58fdd
Nuril <116426536+Bonj3@users.noreply.github.com>
2025-11-24 21:08:20 +0700

    update

--- scr/minimarket/service/ProductService.java
+++ scr/minimarket/service/ProductService.java
@@ -1,1 +1,23 @@
+package minimarket.service;
+
+import minimarket.model.Product;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Service untuk mengelola produk
+ * Menerapkan konsep Collection dan Exception Handling
+ * @author ...
+ */
+public class ProductService {
+    private HashMap<String, Product> products;
+
+    /**
+     * Constructor ProductService
+     */
+    public ProductService() {
+        this.products = new HashMap<>();
+    }
 

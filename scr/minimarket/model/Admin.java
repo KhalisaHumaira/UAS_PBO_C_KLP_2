c747252c00f54c0406a8e11bf72346d6097df7b1
Khalisa Humaira <khalisahumaira242@gmail.com>
2025-11-24 12:12:41 +0700

    Update Admin.java

--- scr/minimarket/model/Admin.java
+++ scr/minimarket/model/Admin.java
@@ -1,1 +1,20 @@
+package minimarket.model;
+
+/**
+- Class Admin yang mewarisi User
+- Menerapkan konsep Inheritance dan Polymorphism
+- @author Kelompok UAS PBO C
+*/
+public class Admin extends User {
+
+    /**
+    - Constructor Admin
+    - @param username Username admin
+    - @param password Password admin
+    - @param name Nama lengkap admin
+    */
+    public Admin(String username, String password, String name) {
+        super(username, password, name);
+    }
+
 

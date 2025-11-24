1ea7c3347e9aa0f45e415a309895915da90a918b
abid r <abidrahmatillah20@gmail.com>
2025-11-24 23:17:56 +0700

    Update CLIHelper.java

--- scr/minimarket/util/CLIHelper.java
+++ scr/minimarket/util/CLIHelper.java
@@ -1,1 +1,29 @@
+package minimarket.util;
 
+import java.util.Scanner;
+
+/**
+ * Utility class untuk membantu tampilan CLI
+ */
+public class CLIHelper {
+    private static Scanner scanner = new Scanner(System.in);
+
+    /**
+     * Clear screen console
+     */
+    public static void clearScreen() {
+        try {
+            final String os = System.getProperty("os.name");
+            if (os.contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (Exception e) {
+            // Jika gagal, print banyak newline
+            for (int i = 0; i < 50; i++) {
+                System.out.println();
+            }
+        }
+    }

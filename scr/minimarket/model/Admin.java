b0b3f9b5bfc4c868c5565a3567c3efdbd9970e87
Khalisa Humaira <khalisahumaira242@gmail.com>
2025-11-24 12:12:59 +0700

    Update Admin.java

--- scr/minimarket/model/Admin.java
+++ scr/minimarket/model/Admin.java
@@ -16,5 +16,15 @@ public class Admin extends User {
     public Admin(String username, String password, String name) {
         super(username, password, name);
     }
-
+    
+    /**
+    - Override method getRole dari User
+    - Menerapkan Polymorphism
+    - @return Role admin
+    */
+    @Override
+    public String getRole() {
+        return "Admin";
+    }
+}
 

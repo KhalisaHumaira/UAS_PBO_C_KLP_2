2b9fff0601458ae617af36bd8f3ea6917f173e6d
Fachraja <fachrajaramadhansukma@gmail.com>
2025-11-24 14:18:32 +0700

    Create Transaction class for transaction management
    Added Transaction class to represent transactions with encapsulation and collection.

--- scr/minimarket/model/Transaction.java
+++ scr/minimarket/model/Transaction.java
@@ -1,1 +1,19 @@
+package minimarket.model;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Class Transaction untuk merepresentasikan transaksi
+ * Menerapkan konsep Encapsulation dan Collection
+ * @author Kelompok UAS PBO C
+ */
+public class Transaction {
+    private String transactionId;
+    private String username;
+    private Date date;
+    private List<TransactionItem> items;
+    private int totalPrice;
 

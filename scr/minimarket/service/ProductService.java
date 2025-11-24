a75112745599281324c3d7ae943eaaecc587b965
Nuril <116426536+Bonj3@users.noreply.github.com>
2025-11-24 21:14:55 +0700

    updatee

--- scr/minimarket/service/ProductService.java
+++ scr/minimarket/service/ProductService.java
@@ -21,3 +21,40 @@ public class ProductService {
         this.products = new HashMap<>();
     }
 
+    /**
+     * Menambah produk baru
+     * @param product Produk yang akan ditambahkan
+     * @return true jika berhasil
+     */
+    public boolean addProduct(Product product) {
+        try {
+            if (products.containsKey(product.getId())) {
+                return false;
+            }
+            products.put(product.getId(), product);
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error menambah produk: " + e.getMessage());
+            return false;
+        }
+    }
+
+    /**
+     * Update produk
+     * @param product Produk yang akan diupdate
+     * @return true jika berhasil
+     */
+    public boolean updateProduct(Product product) {
+        try {
+            if (!products.containsKey(product.getId())) {
+                return false;
+            }
+            products.put(product.getId(), product);
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error update produk: " + e.getMessage());
+            return false;
+        }
+    }
+
+

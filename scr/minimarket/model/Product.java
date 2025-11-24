dfdd17c895913fb146a3afc2e76d4c9001cf57db
Fachraja <fachrajaramadhansukma@gmail.com>
2025-11-24 14:10:06 +0700

    Implement Product class with encapsulation
    Class Product represents a product in the minimarket with encapsulation.

--- scr/minimarket/model/Product.java
+++ scr/minimarket/model/Product.java
@@ -1,1 +1,71 @@
+package minimarket.model;
+
+/**
+ * Class Product untuk merepresentasikan produk di minimarket
+ * Menerapkan konsep Encapsulation
+ * @author Kelompok UAS PBO C
+ */
+public class Product {
+    private String id;
+    private String name;
+    private int price;
+    private int stock;
+    private String category;
+
+    /**
+     * Constructor Product
+     * @param id ID produk
+     * @param name Nama produk
+     * @param price Harga produk
+     * @param stock Stok produk
+     * @param category Kategori produk
+     */
+    public Product(String id, String name, int price, int stock, String category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.category = category;
+    }
+
+    // Getter dan Setter (Encapsulation)
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 

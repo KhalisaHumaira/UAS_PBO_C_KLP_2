ca71a83d0d26845ea149cbeb4f8b1b9d124f8b65
Khalisa Humaira <khalisahumaira242@gmail.com>
2025-11-24 12:14:53 +0700

    Implement Customer class with constructor and role method
    Added constructor and overridden getRole method.

--- scr/minimarket/model/Customer.java
+++ scr/minimarket/model/Customer.java
@@ -6,5 +6,23 @@ package minimarket.model;
  */
 public class Customer extends User {
 
- 
+    /**
+     * Constructor Customer
+     * @param username Username customer
+     * @param password Password customer
+     * @param name Nama lengkap customer
+     */
+    public Customer(String username, String password, String name) {
+        super(username, password, name);
+    }
 
+    /**
+     * Override method getRole dari User
+     * Menerapkan Polymorphism
+     * @return Role customer
+     */
+    @Override
+    public String getRole() {
+        return "Customer";
+    }
+}
